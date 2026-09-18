769dcde7a2a8628419c670b69e8168e53db2cfb6
HERMED-ASOCIADOS <luchohm25@gmail.com>
2026-09-18 18:13:27 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+// =================================
+// MENU MOVIL HERMED
+// =================================
 
+
+const toggle = document.querySelector(".menu-toggle");
+
+const mobileMenu = document.querySelector(".mobile-menu");
+
+
+
+toggle.addEventListener("click", ()=>{
+
+
+mobileMenu.classList.toggle("active");
+
+
+});
