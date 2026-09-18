356b50463d5a447f4510eeca7b4c19ace13e19a5
HERMED-ASOCIADOS <luchohm25@gmail.com>
2026-09-18 18:53:37 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,16 +3,47 @@
 // =================================
 
 
+// =================================
+// MENU MOVIL HERMED PREMIUM
+// =================================
+
+
 const toggle = document.querySelector(".menu-toggle");
 
 const mobileMenu = document.querySelector(".mobile-menu");
 
+const links = document.querySelectorAll(".mobile-menu a");
 
+
+
+// Abrir / cerrar menú
 
 toggle.addEventListener("click", ()=>{
 
 
 mobileMenu.classList.toggle("active");
+
+document.body.classList.toggle("menu-open");
+
+
+});
+
+
+
+// Cerrar al seleccionar una opción
+
+links.forEach(link=>{
+
+
+link.addEventListener("click",()=>{
+
+
+mobileMenu.classList.remove("active");
+
+document.body.classList.remove("menu-open");
+
+
+});
 
 
 });
